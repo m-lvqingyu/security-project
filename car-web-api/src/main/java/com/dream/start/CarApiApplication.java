0143c584efabef92a319dbde2bff164d1b939fc9
lvqingyu <759058217@qq.com>
2020-09-12 22:42:35 +0800

    Security Project Init

---
+++ car-web-api/src/main/java/com/dream/start/CarApiApplication.java
@@ -0,0 +1,17 @@
+package com.dream.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create By 2020/9/12
+ *
+ * @author lvqingyu
+ */
+@SpringBootApplication
+public class CarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarApiApplication.class, args);
+    }
+}
